ec5480eaba7100a102c409377e4c0b64273e0135
Andres Ruiz <aruiz@grupooctano.com.mx>
2020-01-06 09:56:22 -0600

    Clase 1 - Variables

---
+++ clase1.js
@@ -0,0 +1,8 @@
+//console.log('HOLA MUNDO JS')
+var nombre = 'Andrés', apellido = 'Ruiz'
+var edad = 28
+edad = '28 años'
+console.log('Hola ' + nombre + ' ' + apellido )
+console.log('Tengo ' + edad )
+
+peso = 75
