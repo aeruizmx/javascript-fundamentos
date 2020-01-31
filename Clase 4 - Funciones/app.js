15c11646c5786b9034216b52ec29709de58db23e
Andres Ruiz <aruiz@grupooctano.com.mx>
2020-01-31 11:48:41 -0600

    Clase 4 - Funciones

---
+++ Clase 4 - Funciones/app.js
@@ -0,0 +1,10 @@
+var nombre = 'Andres', edad = 32
+
+function imprimirEdad(n, e) {
+  console.log(`${n} tiene ${e} años`)
+}
+
+imprimirEdad(nombre, edad)
+imprimirEdad('Maria',90)
+imprimirEdad(25, 'Jose')
+imprimirEdad()
