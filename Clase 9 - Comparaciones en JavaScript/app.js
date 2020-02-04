13fdef7398c780745d3be08b59f34b7a4ce87a18
Andres Ruiz <aruiz@grupooctano.com.mx>
2020-02-04 09:45:44 -0600

    Clase 9 Comparaciones en js

---
+++ Clase 9 - Comparaciones en JavaScript/app.js
@@ -0,0 +1,13 @@
+var x = 4, y = '4'
+console.log( x == y)
+console.log( x === y)
+
+var andres = {
+  nombre : 'Andres'
+}
+
+var otraPersona = {
+  nombre : 'Sacha'
+}
+console.log(andres == otraPersona )
+console.log(andres === otraPersona )
