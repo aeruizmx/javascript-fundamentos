a97b055c2e09df2165979ffa9c8d2f18c147c5f9
Andres Ruiz <aruiz@grupooctano.com.mx>
2020-02-17 16:37:04 -0600

    Clase 22 Quien es this

---
+++ Clase 22 - Modificar Prototipos/app.js
@@ -0,0 +1,22 @@
+function Persona (nombre, apellido, altura) {
+  this.nombre = nombre
+  this.apellido = apellido
+  this.altura = altura
+}
+
+Persona.prototype.saludar = function () {
+  console.log(`Hola me llamo ${this.nombre} ${this.apellido}`)
+}
+Persona.prototype.soyAlto = function () {
+  return this.altura  > 1.8
+}
+var andres = new Persona('Andres', 'Ruiz', 1.82)
+andres.saludar()
+andres.soyAlto()
+var erika = new Persona('Erika', 'Luna', 1.75)
+erika.saludar()
+erika.soyAlto()
+var arturo = new Persona('Arturo', 'Martinez', 1.56)
+arturo.saludar()
+arturo.soyAlto()
+
