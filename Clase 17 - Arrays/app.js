b4d5109e163f26ed9fa1dfe18e8edf3ec900d6e0
Andres Ruiz <aruiz@grupooctano.com.mx>
2020-02-17 14:36:46 -0600

    Clase 17 Arrays

---
+++ Clase 17 - Arrays/app.js
@@ -0,0 +1,43 @@
+var andres = {
+  nombre: 'Andres',
+  apellido: 'Ruiz',
+  altura: 1.85
+}
+
+var alan = {
+  nombre: 'Alan',
+  apellido: 'Perez',
+  altura: 1.86
+}
+
+var martin = {
+  nombre: 'Martin',
+  apellido: 'Gomezz',
+  altura: 1.85
+}
+
+var dario = {
+  nombre: 'Dario',
+  apellido: 'Juarez',
+  altura: 1.72
+}
+
+var vicky = {
+  nombre: 'Vicky',
+  apellido: 'Zapata',
+  altura: 1.56
+}
+
+var paula = {
+  nombre: 'Paula',
+  apellido: 'Barros',
+  altura: 1.76
+}
+
+var personas = [andres, alan, martin, dario, vicky, paula]
+console.log(personas)
+
+for( var i = 0; i < personas.length; i++){
+  var persona = personas[i]
+  console.log(`${persona.nombre} mide ${persona.altura}mts`)
+}
