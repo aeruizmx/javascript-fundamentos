37b09a0e95f7f8273405afe0e857f8239385e526
Andres Ruiz <aruiz@grupooctano.com.mx>
2020-02-19 10:33:53 -0600

    Clase 27 Callbacks

---
+++ Clase 27 - Callbacks/app.js
@@ -0,0 +1,8 @@
+const API_URL = 'https://swapi.co/api/'
+const PEOPLE_URL = 'people/:id'
+const LUKE_URL = `${API_URL}${PEOPLE_URL.replace(':id',1)}`
+const OPTS = { crossDomain: true}
+const onPeopleResponse =  function(person){
+  console.log(`Hola yo soy ${person.name}`)
+}
+$.get(LUKE_URL, OPTS, onPeopleResponse)
